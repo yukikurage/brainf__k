64b54e517edb286ab5123b3432eda1b27f3afd08
yukikurage <55534323+yukikurage@users.noreply.github.com>
2022-03-15 01:42:57 +0900

    fix output wrap

--- src/Brainfk/System/Transpile.purs
+++ src/Brainfk/System/Transpile.purs
@@ -5,9 +5,9 @@ import Prelude
 import Brainfk.System.Data.BrainfkAST (BrainfkAST(..), Command(..), Statement(..))
 import Brainfk.Util (setZeroTimeout)
 import Control.Promise (Promise, toAff)
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
-import Effect.Aff (Aff)
-import Effect.Class.Console (log)
+import Effect.Aff (Aff, Error)
 
 type Settings r =
   ( memorySize :: Int
@@ -19,12 +19,14 @@ type Settings r =
 defaultSettings :: Record (Settings ())
 defaultSettings =
   { memorySize: 256
-  , chunkNum: 10000
+  , chunkNum: 200000
   , cellSize: 256
   }
 
 foreign import exec_
-  :: Effect Unit
+  :: forall a
+   . (a -> Maybe a)
+  -> Maybe a
   -> (Effect Unit -> Effect Unit)
   -> String
   -> Effect
@@ -32,7 +34,7 @@ foreign import exec_
        , getStep :: Effect Int
        , stop :: Effect Unit
        , getMemory :: Effect (Array Int)
-       , waitFinish :: Promise Unit
+       , waitFinish :: Promise (Maybe Error)
        }
 
 exec
@@ -45,13 +47,12 @@ exec
        , getOutput :: Effect String
        , getStep :: Effect Int
        , stop :: Effect Unit
-       , waitFinish :: Aff Unit
+       , waitFinish :: Aff (Maybe Error)
        }
 exec settings brainfkAST input = do
   let
     transpiled = transpile settings brainfkAST input
-  log transpiled
-  res@{ waitFinish } <- exec_ (pure unit)
+  res@{ waitFinish } <- exec_ Just Nothing
     setZeroTimeout
     transpiled
   pure res { waitFinish = toAff waitFinish }
@@ -61,7 +62,6 @@ transpile settings (BrainfkAST statement) input = tPrelude settings input
   <> tStatement
     settings
     statement
-    true
   <> tReturn
 
 tPrelude :: forall r. Record (Settings r) -> String -> String
@@ -89,50 +89,49 @@ const setZeroTimeoutPromise = () => new Promise((resolve) => {
 const wait = (async () => {
 """
 
-tStatement :: forall r. Record (Settings r) -> Statement -> Boolean -> String
-tStatement _ (StatementEnd) true = "callback();"
-tStatement _ (StatementEnd) false = ""
+tStatement :: forall r. Record (Settings r) -> Statement -> String
+tStatement _ (StatementEnd) = ""
 tStatement
   settings@{ memorySize, cellSize, chunkNum }
-  (StatementCont command statement)
-  isGlobal =
+  (StatementCont command statement) =
   case command of
     PointerIncrement _ n -> "pointer=mod(pointer+" <> show n <> ","
       <> show memorySize
       <> ");step+="
       <> show n
       <> ";chunk+=1;"
-      <> tStatement settings statement isGlobal
+      <> tStatement settings statement
     PointerDecrement _ n -> "pointer=mod(pointer-" <> show n <> ","
       <> show memorySize
       <> ");step+="
       <> show n
       <> ";chunk+=1;"
-      <> tStatement settings statement isGlobal
+      <> tStatement settings statement
     ReferenceIncrement _ n ->
       "memory[pointer]=mod(memory[pointer]+" <> show n <> "," <> show cellSize
         <> ");step+="
         <> show n
         <> ";chunk+=1;"
-        <> tStatement settings statement isGlobal
+        <> tStatement settings statement
     ReferenceDecrement _ n ->
       "memory[pointer]=mod(memory[pointer]-" <> show n <> "," <> show cellSize
         <> ");step+="
         <> show n
         <> ";chunk+=1;"
-        <> tStatement settings statement isGlobal
+        <> tStatement settings statement
     Input _ ->
-      "memory[pointer]=input.codePointAt(inputIndex);inputIndex++;step+=1;chunk+=1;"
-        <> tStatement settings statement isGlobal
+      "if(inputIndex>=input.length){throw new Error('Exceeds Input Range')};memory[pointer]=input.codePointAt(inputIndex);inputIndex++;step+=1;chunk+=1;"
+        <> tStatement settings statement
     Output _ ->
       "output+=String.fromCodePoint(memory[pointer]);step+=1;chunk+=1;"
-        <> tStatement settings statement isGlobal
+        <> tStatement settings statement
     Loop _ loopStatement ->
-      "while(memory[pointer]!==0){if(chunk>=" <> show chunkNum
-        <> "){chunk=0;await setZeroTimeoutPromise();};"
-        <> tStatement settings loopStatement false
+      "while(memory[pointer]!==0){chunk+=1;if(chunk>=" <> show chunkNum
+        <>
+          "){chunk=0;if(isStop){throw new Error('Process Killed')};await setZeroTimeoutPromise();};"
+        <> tStatement settings loopStatement
         <> "};"
-        <> tStatement settings statement isGlobal
+        <> tStatement settings statement
 
 tReturn :: String
 tReturn =
